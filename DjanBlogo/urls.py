2073c4bb3656632a9dd616ee2663d15137261f45
Ashkan Kamyab <ashkankamyab@gmail.com>
2018-10-17 11:22:09 +0200

    Mezzanin Blog function call fixed

--- DjanBlogo/urls.py
+++ DjanBlogo/urls.py
@@ -39,7 +39,7 @@ urlpatterns += [
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
-    # url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
     # ---------------------------------------------
@@ -55,7 +55,7 @@ urlpatterns += [
     # should be used if you want to customize the homepage's template.
     # NOTE: Don't forget to import the view function too!
 
-    url("^$", mezzanine.pages.views.page, {"slug": "/"}, name="home"),
+    # url("^$", mezzanine.pages.views.page, {"slug": "/"}, name="home"),
 
     # HOMEPAGE FOR A BLOG-ONLY SITE
     # -----------------------------
@@ -66,7 +66,7 @@ urlpatterns += [
     # page tree in the admin if it was installed.
     # NOTE: Don't forget to import the view function too!
 
-    # url("^$", blog_views.blog_post_list, name="home"),
+    url("^$", blog_views.blog_post_list, name="home"),
 
     # MEZZANINE'S URLS
     # ----------------
